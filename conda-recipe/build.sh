c43fb3753b5bed731af20d134538203f5d0febb0
Michael Simonelli <michael@simo.live>
2019-03-28 07:31:26 +0000

    update conda-recipe

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-#conda install -c kx embedPy
 
 pip install git+https://github.com/dadadel/pyment.git
 
 export QHOME=$PREFIX/q
-QLIBDIR=l64
 
 mkdir -p $QHOME
-mkdir -p $QHOME/$QLIBDIR
 mv extend.q reflect.p $QHOME
